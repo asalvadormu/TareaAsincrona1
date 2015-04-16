029c077e71f15514ca44e8be6af63e0fdcc74723
SAMUAN <asalvadormu@gmail.com>
2015-04-16 08:51:13 +0200

    commit 1

--- app/src/main/java/com/samuan/assignament3/MainActivity.java
+++ app/src/main/java/com/samuan/assignament3/MainActivity.java
@@ -2,6 +2,7 @@ package com.samuan.assignament3;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,8 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.i("TAG", "inicio");
         new Tarea1().execute("a");
+        Log.i("TAG","despues");
     }
 
 
