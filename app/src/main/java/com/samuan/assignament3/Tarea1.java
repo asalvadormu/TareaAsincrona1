a390c38a3edd5a52e5bf12b2ef813109c2d808f9
SAMUAN <asalvadormu@gmail.com>
2015-04-16 10:38:05 +0200

    cambio final

--- app/src/main/java/com/samuan/assignament3/Tarea1.java
+++ app/src/main/java/com/samuan/assignament3/Tarea1.java
@@ -8,34 +8,7 @@ import android.util.Log;
  */
 public class Tarea1 extends AsyncTask<String, Integer, String>{
 
-    /**
-     this not problem just take and store somewhere parameter from onPostExecute
-     of authTask and when authTask finished pass it to imageTask**/
-
-    /**if(authTask .getStatus() == AsyncTask.Status.PENDING){
-     // My AsyncTask has not started yet
-     }
-
-     if(authTask .getStatus() == AsyncTask.Status.RUNNING){
-     // My AsyncTask is currently doing work in doInBackground()
-     }
-
-     if(authTask .getStatus() == AsyncTask.Status.FINISHED){
-     // START NEW TASK HERE
-     }**/
-
-
-    /**authTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
-     // Image task will only be done AFTER textViewTask is done
-     imageTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
-     And for newer versions a simple
-
-     ...
-     // ICS+ and pre honeycomb (I think)
-     authTask.execute();
-     // Image task will only be done AFTER textViewTask is done
-     imageTask.execute();
-     ...**/
+    public RespuestaAsincrona delegate=null;
 
     @Override
     protected String doInBackground(String[] params) {
