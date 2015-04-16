614fca05444927c4fd69568930924f1526d0dd9b
SAMUAN <asalvadormu@gmail.com>
2015-04-16 08:55:41 +0200

    commit 2

---
+++ app/src/main/java/com/samuan/assignament3/Tarea2.java
@@ -0,0 +1,36 @@
+package com.samuan.assignament3;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+/**
+ * Created by SAMUAN on 16/04/2015.
+ */
+public class Tarea2 extends AsyncTask<String,Integer,String>{
+
+    @Override
+    protected String doInBackground(String[] params) {
+        for(int i=0;i<5;i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return "terminado2";
+    }
+
+    @Override
+    protected void  onPreExecute(){}
+
+    @Override
+    protected void onProgressUpdate(Integer... progress) {
+
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Log.i("ALGO", "EL RESULTADO de 2 " + result);
+    }
+}
